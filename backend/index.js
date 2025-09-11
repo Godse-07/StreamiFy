42354042278d93321b2e6371c64d18572d68805a
Pushan Mukhopadhyay <mukhopadhyaypushan42@gmail.com>
2025-09-12 03:08:18 +0530

    update deploy

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,39 @@
-require('dotenv').config();
-const express = require('express');
-const router = require('./routes/authRoute');
-const mongoConnection = require('./config/db');
-const cookieParser = require('cookie-parser');
-const userRoutes = require('./routes/userRoutes');
-const chatRoutes = require('./routes/chatRoutes');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const router = require("./routes/authRoute");
+const mongoConnection = require("./config/db");
+const cookieParser = require("cookie-parser");
+const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 app.use(cookieParser());
-
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:5173",
     credentials: true,
-}))
+  })
+);
 
-app.use("/api/auth", router)
-
+app.use("/api/auth", router);
 app.use("/api/users", userRoutes);
-
 app.use("/api/chat", chatRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
-mongoConnection().then(()=>{
-    app.listen(3000, ()=>{
-        console.log('Server is running on port 3000');
-    })
-})
+mongoConnection().then(() => {
+  app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+  });
+});
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+const baseUrl = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api";
+
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: baseUrl,
     withCredentials: true 
 })
